e90f0bff4802090c693fb026f485b6d78a9b0756
Nguyễn Hoàng Anh <52200067@student.tdtu.edu.vn>
2025-04-02 21:43:45 +0700

    fix splash screen and title app

--- app/lib/screens/splash_screen.dart
+++ app/lib/screens/splash_screen.dart
@@ -23,7 +23,7 @@ class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderSt
 
     _animation = CurvedAnimation(parent: _controller, curve: Curves.easeIn);
 
-    Timer(Duration(seconds: 5), () {
+    Timer(Duration(seconds: 3), () {
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (context) => HomeScreen()),

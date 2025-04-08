807d5d5b29080ea85d4db56ae5fd3ecce426b921
Tuan NGUYEN <babycutevt112304@gmail.com>
2025-04-08 21:41:58 +0700

    loginsignin

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,37 +1,21 @@
 import 'package:flutter/material.dart';
-import 'screens/splash_screen.dart';
-import 'screens/home_screen.dart';
-import 'theme/app_theme.dart';
+
+import 'screens/signupScreen.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Smart Lock',
       theme: ThemeData(
-        fontFamily: 'Roboto',
-        primaryColor: Color(0xFF0F0F0F),
-        scaffoldBackgroundColor: Colors.white,
-        appBarTheme: AppBarTheme(
-          backgroundColor: Colors.white,
-          elevation: 0,
-          iconTheme: IconThemeData(color: Color(0xFF0F0F0F)),
-          titleTextStyle: TextStyle(
-            color: Color(0xFF0F0F0F),
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
+        primarySwatch: Colors.blue,
       ),
-      home: SplashScreen(),
-      routes: {
-        '/home': (context) => HomeScreen(),
-      },
+      home: const SignUp(),
     );
   }
 }

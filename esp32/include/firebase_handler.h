3e663ab48b8c81b9b91eed192fd8563e50d91d44
Puahuhu <nguyenletuanphuongzzz@gmail.com>
2025-04-13 16:44:32 +0700

    Update firebase_handler.h

--- esp32/include/firebase_handler.h
+++ esp32/include/firebase_handler.h
@@ -9,5 +9,7 @@ void firebaseSetup();
 void firebaseLoop(const String& lockId);
 void putOpenHistory(const String& uuid, const String& lockId, const String& method, const String& device);
 void putWarningHistory(const String& uuid, const String& lockId, const String& message);
+void deletePinCodeDisable(const String& lockId);
+bool checkPinCodeEnable(const String& lockId);
 
 #endif
